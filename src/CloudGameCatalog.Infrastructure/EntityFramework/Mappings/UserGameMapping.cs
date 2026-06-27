5f325db4d17d8d2ec405f3b9402db223ec9fe2e8
Igor Tudisco <igortudisco@hotmail.com>
2026-06-27 11:19:32 -0300

    feat(db): adiciona entidade UserGame e mapeamento do EF Core
    Introdução da entidade UserGame para representar relacionamentos entre
    usuário e jogo. Adição do UserGameMapping para o EF Core, mapeando para
    a tabela "UserGames" com um índice único em (IdUser, IdGame) e campos
    obrigatórios.

---
+++ src/CloudGameCatalog.Infrastructure/EntityFramework/Mappings/UserGameMapping.cs
@@ -0,0 +1,17 @@
+﻿using CloudGameCatalog.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CloudGameCatalog.Infrastructure.EntityFramework.Mappings;
+
+public class UserGameMapping : IEntityTypeConfiguration<UserGame>
+{
+    public void Configure(EntityTypeBuilder<UserGame> builder)
+    {
+        builder.ToTable("UserGames");
+        builder.HasKey(k => k.Id);
+        builder.HasIndex(x => new { x.IdUser, x.IdGame }).IsUnique();
+        builder.Property(x => x.IdUser).IsRequired();
+        builder.Property(x => x.IdGame).IsRequired();
+    }
+}
