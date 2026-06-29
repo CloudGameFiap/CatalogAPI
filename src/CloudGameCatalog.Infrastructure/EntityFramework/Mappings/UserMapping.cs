f92976c4264888d2e0b2b2d91d563174304518df
Guilherme Paschoal <paschoal.gui@gmail.com>
2026-06-29 15:36:32 -0300

    feat: add mappings for users and usergames

--- src/CloudGameCatalog.Infrastructure/EntityFramework/Mappings/UserMapping.cs
+++ src/CloudGameCatalog.Infrastructure/EntityFramework/Mappings/UserMapping.cs
@@ -8,7 +8,7 @@ public sealed class UserMapping : IEntityTypeConfiguration<User>
 {
     public void Configure(EntityTypeBuilder<User> builder)
     {
-        builder.ToTable("Users", x => x.ExcludeFromMigrations());
+        builder.ToTable("Users");
         builder.HasKey(k => k.Id);
         builder.Property(p => p.Name).IsRequired().HasColumnType("VARCHAR(120)");
         builder.Property(p => p.Email).IsRequired().HasColumnType("VARCHAR(120)");
